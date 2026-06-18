fdcd2a22d7112d58973014ee4dc617f663adf84e
Benjamin Abendroth <braph93@gmx.de>
2026-06-18 17:53:19 +0100

    Make zitat work on debian

--- plugins/zitat.py
+++ plugins/zitat.py
@@ -1,8 +1,31 @@
 #!/usr/bin/env python3
 
+import os
 import subprocess
 
+def _find_fortune_bin():
+    candidates = [
+        f'/usr/games/fortune',
+        f'/usr/bin/fortune'
+    ]
+
+    for file in candidates:
+        if os.path.exists(file):
+            return file
+
+def _find_fortune_file(topic):
+    candidates = [
+        f'/usr/share/games/fortunes/de/{topic}',
+        f'/usr/share/fortunes/{topic}'
+    ]
+
+    for file in candidates:
+        if os.path.exists(file):
+            return file
+
 def zitat(chatbot, args, message):
-    r = subprocess.run(['fortune', 'zitate'], stdout=subprocess.PIPE)
+    fortune_bin = _find_fortune_bin()
+    zitate_file = _find_fortune_file('zitate')
+    r = subprocess.run([fortune_bin, zitate_file], stdout=subprocess.PIPE)
     r = r.stdout.decode('UTF-8')
     chatbot.send_message(r)
